f9981173e3d2b408d5d873a6f4b2d49767b7b6c0
copyface <kenthendico011@gmail.com>
2025-11-13 13:41:30 +0800

    feat: Added UI

---
+++ rps.js
@@ -0,0 +1,76 @@
+// score variables
+let humanScore = 0;
+let computerScore = 0;
+const choices = ["rock", "paper", "scissors"];
+
+// get the computers choice
+function getComputerChoice() {
+  const random = Math.floor(Math.random() * choices.length);
+  return choices[random];
+}
+
+// play the round
+function playRound(humanChoice, computerChoice) {
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    console.log(
+      "human: " + humanChoice + " vs " + "computer: " + computerChoice,
+    );
+    console.log("You Won! " + humanChoice + " beats " + computerChoice + ".");
+    return;
+  } else if (humanChoice === computerChoice) {
+    console.log(
+      "human: " + humanChoice + " vs " + "computer: " + computerChoice,
+    );
+    console.log("Draw! both chose " + humanChoice + ".");
+    return;
+  } else {
+    computerScore++;
+    console.log(
+      "human: " + humanChoice + " vs " + "computer: " + computerChoice,
+    );
+    console.log("You Lose! " + computerChoice + " beats " + humanChoice + ".");
+    return;
+  }
+}
+
+let playerChoice = null;
+const options = document.querySelector(".card");
+options.addEventListener("click", (event) => {
+  const choice = event.target;
+
+  switch (true) {
+    case choice.className.includes("card__button--rock"):
+      playerChoice = "rock";
+      break;
+    case choice.className.includes("card__button--paper"):
+      playerChoice = "paper";
+      break;
+    case choice.className.includes("card__button--scissors"):
+      playerChoice = "scissors";
+      break;
+  }
+
+  if (playerChoice) {
+    console.log("Player selected: " + playerChoice);
+  }
+});
+
+// play the game
+function playGame() {
+  // const random = Math.floor(Math.random() * choices.length);
+}
+
+const menuButton = document.querySelector("#menu__button");
+
+function mainMenu() {
+  const menu = document.querySelector("#menu");
+  menu.remove();
+  setTimeout(() => playGame(), 3000);
+}
+
+menuButton.addEventListener("click", mainMenu);
